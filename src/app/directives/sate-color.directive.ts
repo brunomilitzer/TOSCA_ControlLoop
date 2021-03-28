0c82035a2560e74293b31773e5465ebe839cb6dc
brunomilitzer <bruno.militzer@est.tech>
2021-03-28 17:00:29 +0100

    Fixed Sate Directives

--- src/app/directives/sate-color.directive.ts
+++ src/app/directives/sate-color.directive.ts
@@ -5,30 +5,49 @@ import { ControlLoopState } from '../models/enums/cl-state.enum';
   selector: '[appStateColor]'
 } )
 export class SateColorDirective implements OnInit {
-  @HostBinding( 'class' ) elementClass = 'btn btn-unitialised';
   @Input() state: ControlLoopState;
+
+  protected elClass: string[] = [];
+
+  @Input( 'class' )
+  @HostBinding( 'class' )
+  get elementClass(): string {
+    return this.elClass.join( ' ' );
+  }
+
+  set( val: string ): void {
+    this.elClass = val.split( ' ' );
+  }
 
   constructor( private elRef: ElementRef, private renderer: Renderer2 ) {
   }
 
   ngOnInit(): void {
-    console.log( this.state );
-    this.renderer.addClass( this.elRef.nativeElement, this.getStatus() );
+    this.renderer.addClass( this.elRef.nativeElement, this.elClass[1] );
+
+    this.checkState();
   }
 
-  private getStatus(): string {
+  private checkState(): void {
+    this.elClass.push( 'btn' );
+
     switch ( this.state ) {
       case ControlLoopState.UNINITIALISED:
-        console.log( 'Unitialised' );
-        return 'btn btn-unitialised';
+        this.elClass.push( 'btn-unitialised' );
+        console.log( 'Current State: unitialised' );
+        break;
       case ControlLoopState.PASSIVE:
-        console.log( 'Passive' );
-        return 'btn-passive';
+        this.elClass.push( 'btn-passive' );
+        console.log( 'Current State: passive' );
+        break;
       case ControlLoopState.RUNNING:
-        console.log( 'Running' );
-        return 'btn-running';
+        this.elClass.push( 'btn-running' );
+        console.log( 'Current State: running' );
+        break;
       default:
-        return 'btn-unitialised';
+        this.elClass.push( 'btn-default' );
+        console.log( 'Current State: default' );
+        break;
     }
   }
 
