e2fd60c13ca1053eabe2e467a881292427360e04
brunomilitzer <bruno.militzer@est.tech>
2021-04-13 08:28:47 +0100

    Fixed Alert Position

--- src/app/core/interceptors/global-error.interceptor.ts
+++ src/app/core/interceptors/global-error.interceptor.ts
@@ -12,7 +12,6 @@ export class GlobalErrorInterceptor implements ErrorHandler {
 
     this.logger.error( '======== Global Error Interceptor ========' );
     this.logger.error( error.message );
-    this.logger.log( error );
 
     this.alertService.error( error.message );
   }
