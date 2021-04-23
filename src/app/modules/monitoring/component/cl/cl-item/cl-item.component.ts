6f7e578ebbd46cd47557f7630700dddfb47c65d2
brunomilitzer <bruno.militzer@est.tech>
2021-04-23 17:24:37 +0100

    Created Universal Modal

--- src/app/modules/monitoring/component/cl/cl-item/cl-item.component.ts
+++ src/app/modules/monitoring/component/cl/cl-item/cl-item.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ControlLoop } from '../../../../../models/cl.model';
 import { LogService } from '../../../../../shared/logging/service/log.service';
 import { ControlLoopList } from '../../../../../models/cl-list.model';
+import { ModalService } from '../../../../../shared/modal/services/modal.service';
 
 @Component( {
   selector: 'tosca-cl-item',
@@ -16,11 +17,19 @@ export class ClItemComponent implements OnInit, OnDestroy {
   @Input() title: string;
   public isCollapsed = false;
 
-  constructor( private logger: LogService ) {
+  constructor( private logger: LogService, private modalService: ModalService ) {
   }
 
   ngOnInit(): void {
     this.logger.debug( '======== Cl Item Component Init ========' );
+  }
+
+  openModal( id: string ): void {
+    this.modalService.open( id );
+  }
+
+  closeModal( id: string ): void {
+    this.modalService.close( id );
   }
 
   ngOnDestroy(): void {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertComponent } from './alert/component/alert.component';
 import { LogTestComponent } from './logging/test/log-test.component';
+import { ModalComponent } from './modal/component/modal.component';
 
 @NgModule( {
   declarations: [
     AlertComponent,
-    LogTestComponent
+    LogTestComponent,
+    ModalComponent
   ],
   imports: [ CommonModule ],
   exports: [
     AlertComponent,
-    LogTestComponent
+    LogTestComponent,
+    ModalComponent
   ]
 } )
 export class SharedModule {
